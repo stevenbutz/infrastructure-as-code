06d18df93cd49b5c06f8820bb0e70e13a6142554
stebutz <stebutz@iu.edu>
2024-04-18 18:05:04 -0400

    16

--- resource-group.tf
+++ resource-group.tf
@@ -2,6 +2,6 @@
 # Creates a resource group on azure
 #
 resource "azurerm_resource_group" "practicum16" {
-  name     = "practicum16"
-  location = "eastus"
+  name     = var.app_name
+  location = var.location
 }
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable app_name {
+
+}
+
+variable location {
+    default = "eastus"
+}
+
+variable kubernetes_version {
+    default     = "1.29.2"
+}
